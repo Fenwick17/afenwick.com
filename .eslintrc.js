0775588328892b06c36f15db59603f0549fe0efd
Adam Fenwick <afenwickdev@gmail.com>
2021-01-30 14:54:01 +0000

    Setting up TSLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,25 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     es2021: true,
   },
   extends: [
     'plugin:react/recommended',
-    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript',
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
+    "project": './tsconfig.json',
     ecmaVersion: 12,
     sourceType: 'module',
   },
   plugins: [
     'react',
+    '@typescript-eslint',
   ],
   rules: {
     'react/jsx-filename-extension': [0],
