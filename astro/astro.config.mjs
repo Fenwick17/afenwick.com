9f50513d81b4f034356e07d91300fd59af755f7f
Adam Fenwick <afenwickdev@gmail.com>
2023-10-04 16:52:54 +0100

    Updating toolkit

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,18 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sanity from 'astro-sanity';
 
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sanity({
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: 'production',
-    apiVersion: '2021-03-25',
-    useCdn: true
-  })],
-  output: "server",
-  adapter: vercel()
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sanity({
+      projectId: process.env.SANITY_PROJECT_ID,
+      dataset: 'production',
+      apiVersion: '2021-03-25',
+      useCdn: true,
+    }),
+  ],
 });
