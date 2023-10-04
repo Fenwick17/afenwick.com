f3ef32a976c15dc6ed349278fe98eead6c34e0ff
Adam Fenwick <afenwickdev@gmail.com>
2023-10-04 11:46:05 +0100

    Adding netlify SSR

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,19 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import sanity from 'astro-sanity';
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sanity({
-      projectId: process.env.SANITY_PROJECT_ID,
-      dataset: 'production',
-      apiVersion: '2021-03-25',
-      useCdn: true,
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sanity({
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    useCdn: true
+  })],
+  output: "server",
+  adapter: netlify()
 });
