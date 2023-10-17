823e6bc2abcfcc1f2b0674785be408fbb2374a4e
Adam Fenwick <afenwickdev@gmail.com>
2023-10-17 14:12:07 +0100

    Updating robots and sitemap

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sanity from 'astro-sanity';
+import sitemap from '@astrojs/sitemap';
 
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sanity({
-      projectId: process.env.SANITY_PROJECT_ID,
-      dataset: 'production',
-      apiVersion: '2021-03-25',
-      useCdn: true,
-    }),
-  ],
+  site: 'https://afenwick.com',
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sanity({
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    useCdn: true
+  }), sitemap(), robotsTxt()]
 });
