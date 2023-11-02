f91e9088353c36052c4c8800956497b58c89b21e
Adam Fenwick <afenwickdev@gmail.com>
2023-11-02 14:28:58 +0000

    Change to trigger Vercel

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -7,7 +7,6 @@ import 'dotenv/config';
 
 import robotsTxt from 'astro-robots-txt';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://afenwick.com',
   integrations: [
