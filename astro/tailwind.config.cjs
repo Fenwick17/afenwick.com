e9b12f44227792f98ee054c0fb58a67d88d7594c
Adam Fenwick <afenwickdev@gmail.com>
2023-12-19 16:26:01 +0000

    Adding analytics and speed insights

--- astro/tailwind.config.cjs
+++ astro/tailwind.config.cjs
@@ -4,6 +4,9 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily: {
+      sans: 'Inter',
+    },
     fontFeatureSettings: {
       numeric: ['tnum', 'salt', 'ss02'],
     },
@@ -31,5 +34,4 @@ module.exports = {
     extend: {},
   },
   darkMode: 'class',
-  plugins: [require('tailwindcss-font-inter')],
 };
