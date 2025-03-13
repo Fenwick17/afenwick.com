62c3d0dde8f90034137f37ea390cc1b8634813cf
Adam Fenwick <afenwickdev@gmail.com>
2025-03-13 19:48:02 +0900

    Initial draft for portfolio

--- astro/sanity/api.ts
+++ astro/sanity/api.ts
@@ -1,6 +1,6 @@
 import { useSanityClient, groq } from 'astro-sanity';
 import { formatDate } from '../utils/formatDate';
-import type { BlogPost } from '@types';
+import type { BlogPost, Project } from '@types';
 
 export async function getBlogs(): Promise<[]> {
   const query = groq`*[_type == "blog"] | order(publishedAt desc) {
@@ -40,4 +40,18 @@ export async function getBlogPost(slug: string): Promise<[]> {
   const query = groq`*[_type == "blog" && slug.current == "${slug}"]`;
   const blogPost = await useSanityClient().fetch(query);
   return blogPost[0];
+}
+
+export async function getPortfolio(): Promise<[]> {
+  const query = groq`*[_type == "portfolio"] | order(publishedAt desc) {
+    title,
+    slug,
+    body,
+    publishedAt,
+  }[0...2]`;
+  const projects = await useSanityClient().fetch(query);
+  projects.forEach((project: Project) => {
+    project.publishedAt = formatDate(project.publishedAt);
+  });
+  return projects;
 }
---
+++ sanity/schemas/portfolio.ts
@@ -0,0 +1,60 @@
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+  name: 'portfolio',
+  title: 'Portfolio',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: { type: 'author' },
+    }),
+    defineField({
+      name: 'jobRole',
+      title: 'Role',
+      type: 'string',
+    }),
+    defineField({
+      name: 'projectDate',
+      title: 'Project date',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+    },
+    prepare(selection) {
+      const { author } = selection;
+      return { ...selection, subtitle: author && `by ${author}` };
+    },
+  },
+});
