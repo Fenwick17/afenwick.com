4aa1f34b31b79d49390d968f9d3024d23033d37d
Adam Fenwick <afenwickdev@gmail.com>
2026-09-07 07:14:48 +0100

    Initial pass at upgrades

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,21 +1,28 @@
-import { defineConfig } from 'astro/config';
-import sanity from '@sanity/astro';
-import sitemap from '@astrojs/sitemap';
-import 'dotenv/config';
+import { defineConfig, fontProviders } from "astro/config";
+import sanity from "@sanity/astro";
+import sitemap from "@astrojs/sitemap";
+import "dotenv/config";
 
-import robotsTxt from 'astro-robots-txt';
+import robotsTxt from "astro-robots-txt";
 
 export default defineConfig({
-  site: 'https://afenwick.com',
+  site: "https://afenwick.com",
   integrations: [
     sanity({
       projectId: process.env.SANITY_PROJECT_ID,
-      dataset: 'production',
-      apiVersion: '2021-03-25',
+      dataset: "production",
+      apiVersion: "2021-03-25",
       token: process.env.SANITY_READ_TOKEN,
     }),
     sitemap(),
     robotsTxt(),
   ],
-  output: 'static',
+  output: "static",
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+    },
+  ],
 });
